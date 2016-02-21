d35db25cb52aed1f71324401446879ce94770de2
bridgewaterrobbie <bridgewaterrobbie@gmail.com>
2016-02-20 22:09:21 -0500

    t

---
+++ VR_Project2/Assets/Scripts/FireTrigger.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+var fire : GameObject;
+var spark : GameObject;
+private var myLight : Light;
+public var AudioFile : AudioSource;
+function Start () {
+
+	myLight = GetComponent(Light);
+	fire.SetActive(false);
+	spark.SetActive(false);
+	myLight.intensity = 0;
+	AudioFile.Stop();
+}
+
+function OnTriggerEnter () {
+	fire.SetActive(true);
+	spark.SetActive(true);
+	myLight.intensity = 4;
+	AudioFile.Play();
+}
