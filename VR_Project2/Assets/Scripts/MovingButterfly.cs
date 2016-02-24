11d7f7037a1ff0f8cf91b334b8b4269c72668247
karl kniel <kniel@ufl.com>
2016-02-24 01:17:14 -0500

    stuff

---
+++ VR_Project2/Assets/Scripts/MovingButterfly.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingButterfly : MonoBehaviour 
+{
+
+	public float horizontalspeed;
+	public float verticalspeed;
+	public float aplitude;
+
+	private Vector3 tempPosition;
+
+	void Start () 
+	{
+		tempPosition = transform.position;
+	}
+
+	void FixedUpdate () 
+	{
+		tempPosition.x += horizontalspeed;
+		tempPosition.y = Mathf.Sin (Time.realtimeSinceStartup * verticalspeed) * aplitude;
+		transform.position = tempPosition;
+	}
+}
