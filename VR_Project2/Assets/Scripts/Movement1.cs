4457081d45d4bcd87b160a55304dd64d2391c36d
CraigLu <Craig.Z.Lu@gmail.com>
2016-02-24 15:56:08 -0500

    added fish to river

---
+++ VR_Project2/Assets/Scripts/Movement1.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement1 : MonoBehaviour {
+	public iTween tween;
+	// Use this for initialization
+	void Start () {
+		iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath ("Fish1"), "time", 300));
+		tween.loopType = iTween.LoopType.pingPong;
+	}
+}
