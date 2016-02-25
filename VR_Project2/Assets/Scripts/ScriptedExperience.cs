bc01b2c78b70e9397accb0daade3cd3cbef9ff3b
CraigLu <Craig.Z.Lu@gmail.com>
2016-02-24 22:55:35 -0500

    removed grass and fixed script

--- VR_Project2/Assets/Scripts/ScriptedExperience.cs
+++ VR_Project2/Assets/Scripts/ScriptedExperience.cs
@@ -10,7 +10,10 @@ public class ScriptedExperience : MonoBehaviour {
 	public AudioSource audio5;
 	public AudioSource audio6;
 	public CardboardHead head;
+	private Autowalk walk;
 	IEnumerator Start () {
+		walk = GameObject.FindGameObjectWithTag ("Character").GetComponent<Autowalk>();
+		walk.walkWhenTriggered = false;
 		head = GetComponent<CardboardHead> ();
 		head.trackRotation = false;
 		head.trackPosition = false;
@@ -23,11 +26,13 @@ public class ScriptedExperience : MonoBehaviour {
 		head.trackRotation = true;
 		yield return new WaitForSeconds (audio3.clip.length - 3);
 		audio4.Play ();
-		yield return new WaitForSeconds (8f);
+		yield return new WaitForSeconds (8.1f);
 		audio4.transform.localPosition = new Vector3 (1, 0, 0);
 		audio4.spatialBlend = .8f;
 		audio4.panStereo = .4f;
 		audio4.maxDistance = 50;
 		audio4.minDistance = 3;
+		yield return new WaitForSeconds (audio4.clip.length - 8.1f);
+		walk.walkWhenTriggered = true;
 	}
 }
